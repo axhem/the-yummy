b647681a04763fcc6f35130cd40b1fe448469860
axhem <axhemshahaj@gmail.com>
2020-05-07 22:00:51 +0200

    add deployment config

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+// Serve only the static files form the angularapp directory
+app.use(express.static(__dirname + '/the-yummy'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+res.sendFile(path.join(__dirname+'/the-yummy/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
